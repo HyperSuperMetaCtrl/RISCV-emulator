3af0ce5d30058be8b8211e489744ff40b682e775
Marcel Wiesenhöfer <8881592+HyperSuperMetaCtrl@users.noreply.github.com>
2022-12-13 12:05:22 +0100

    Add sll()

--- main.c
+++ main.c
@@ -206,6 +206,12 @@ void sra(CPU* cpu, const RInstruction* r_instruction) {
 	cpu->pc_ += 4;
 }
 
+void sll(CPU* cpu, const RInstruction* r_instruction) {
+	cpu->regfile_[r_instruction->rd] = cpu->regfile_[r_instruction->rs1]
+		<< cpu->regfile_[r_instruction->rs2];
+	cpu->pc_ += 4;
+}
+
 /**
  * I Instructions
  */
