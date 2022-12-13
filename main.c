bae9a546a55b57b6af828390bdaf13324b026d80
Marcel Wiesenhöfer <8881592+HyperSuperMetaCtrl@users.noreply.github.com>
2022-12-13 12:05:22 +0100

    Add sltu()

--- main.c
+++ main.c
@@ -218,6 +218,12 @@ void slt(CPU* cpu, const RInstruction* r_instruction) {
 	cpu->pc_ += 4;
 }
 
+void sltu(CPU* cpu, const RInstruction* r_instruction) {
+	cpu->regfile_[r_instruction->rd] = cpu->regfile_[r_instruction->rs1]
+		< cpu->regfile_[r_instruction->rs2];
+	cpu->pc_ += 4;
+}
+
 /**
  * I Instructions
  */
