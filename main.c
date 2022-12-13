9bfcd26407efd8fe1f4b9f23fb6c7eb67c4d0ff9
Marcel Wiesenhöfer <8881592+HyperSuperMetaCtrl@users.noreply.github.com>
2022-12-13 12:05:22 +0100

    Add add()

--- main.c
+++ main.c
@@ -164,6 +164,11 @@ RInstruction decode_r_instruction(const uint32_t *instruction) {
 	return r_instr;
 }
 
+void add(CPU* cpu, const RInstruction *r_instruction) {
+	cpu->regfile_[r_instruction->rd] = cpu->regfile_[r_instruction->rs1]
+		+ cpu->regfile_[r_instruction->rs2];
+	cpu->pc_ += 4;
+}
 /**
  * I Instructions
  */
