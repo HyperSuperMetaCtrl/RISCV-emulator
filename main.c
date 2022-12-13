02dc10124e38195e41caa55cd4db64796b8d37bc
Marcel Wiesenhöfer <8881592+HyperSuperMetaCtrl@users.noreply.github.com>
2022-12-13 12:05:22 +0100

    Add xor()

--- main.c
+++ main.c
@@ -188,6 +188,12 @@ void or(CPU* cpu, const RInstruction *r_instruction) {
 	cpu->pc_ += 4;
 }
 
+void xor(CPU* cpu, const RInstruction *r_instruction) {
+	cpu->regfile_[r_instruction->rd] = cpu->regfile_[r_instruction->rs1]
+		^ cpu->regfile_[r_instruction->rs2];
+	cpu->pc_ += 4;
+}
+
 /**
  * I Instructions
  */
