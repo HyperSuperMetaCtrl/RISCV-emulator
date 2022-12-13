d6c3570c141837cb1beeca0ae4b8a5011bda34d5
Marcel Wiesenhöfer <8881592+HyperSuperMetaCtrl@users.noreply.github.com>
2022-12-13 12:05:22 +0100

    Add or()

--- main.c
+++ main.c
@@ -182,6 +182,12 @@ void and(CPU* cpu, const RInstruction *r_instruction) {
 	cpu->pc_ += 4;
 }
 
+void or(CPU* cpu, const RInstruction *r_instruction) {
+	cpu->regfile_[r_instruction->rd] = cpu->regfile_[r_instruction->rs1]
+		| cpu->regfile_[r_instruction->rs2];
+	cpu->pc_ += 4;
+}
+
 /**
  * I Instructions
  */
